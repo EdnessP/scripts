3a71dab8ca0e2fabd2c2208979639ed1cb8c867d
EdnessP <55930127+EdnessP@users.noreply.github.com>
2021-10-23 19:14:21 +0300

    Update ms_timestamp.py

--- other/ms_timestamp.py
+++ other/ms_timestamp.py
@@ -5,8 +5,8 @@
 import argparse
 from datetime import datetime
 
-def print_time(name, time):
-    print(datetime.utcfromtimestamp(time).strftime(f"{name} date: %Y-%m-%d %H:%M:%S"))
+def print_time(type):
+    print(datetime.utcfromtimestamp(int.from_bytes(file.read(4), "big" if type == "XEX/PE" else "little")).strftime(f"{type} date: %Y-%m-%d %H:%M:%S"))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=str)
@@ -14,29 +14,30 @@ args = parser.parse_args()
 
 file = open(args.file, "rb") 
 magic = file.read(4)
+
 if (magic[:2] == b"MZ"):
     file.seek(0x3C, 0)
     file.seek(int.from_bytes(file.read(4), "little") + 0x8, 0)
-    print_time("PE", int.from_bytes(file.read(4), "little"))
+    print_time("PE")
 
 elif (magic == b"XE\x00\x00"):
     file.seek(0x1C, 0)
-    print_time("XE", int.from_bytes(file.read(4), "little"))
+    print_time("XE")
     file.seek(0x74, 0)
-    print_time("PE", int.from_bytes(file.read(4), "little"))
+    print_time("PE")
 
 elif (magic == b"XBEH"):
     file.seek(0x114, 0)
-    print_time("XBE", int.from_bytes(file.read(4), "little"))
+    print_time("XBE")
     file.seek(0x148, 0)
-    print_time("PE", int.from_bytes(file.read(4), "little"))
+    print_time("PE")
     file.seek(0x188, 0)
-    print_time("Cert", int.from_bytes(file.read(4), "little"))
+    print_time("Cert")
 
 elif (magic == b"XEX?" or magic == b"XEX0"):
     file.seek(0x103C, 0)
     file.seek(int.from_bytes(file.read(4)) + 0x1008, 0)
-    print_time("PE", int.from_bytes(file.read(4), "little"))
+    print_time("PE")
 
 elif (magic == b"XEX-" or magic == b"XEX1" or magic == b"XEX2"):
     file.seek(0x14, 0)
@@ -46,7 +47,7 @@ elif (magic == b"XEX-" or magic == b"XEX1" or magic == b"XEX2"):
         offset = int.from_bytes(file.read(4), "big")
         if type == 0x18002:
             file.seek(offset + 0x4)
-            print_time("XEX/PE", int.from_bytes(file.read(4), "big"))
+            print_time("XEX/PE")
             break
 
 else:
