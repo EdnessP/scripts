c0902631874447781d3e50e4e9cd8d383345ce5a
EdnessP <55930127+EdnessP@users.noreply.github.com>
2021-10-27 13:44:00 +0300

    Update ms_timestamp.py

--- other/ms_timestamp.py
+++ other/ms_timestamp.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # Python reimplementation of xbexexmzpe.bms
-# Written by jason098   2021-10-23   v1.1
+# Written by jason098   2021-10-23   v1.2
 
 import argparse
 from datetime import datetime
 
 def print_time(type):
-    timestamp = int.from_bytes(file.read(4), "big" if type == "XEX/PE" else "little")
+    timestamp = int.from_bytes(file.read(0x4), "big" if type == "XEX" else "little")
     print(datetime.utcfromtimestamp(timestamp).strftime(f"{type} date: %Y-%m-%d %H:%M:%S"))
 
 parser = argparse.ArgumentParser()
@@ -17,39 +17,39 @@ file = open(args.file, "rb")
 magic = file.read(4)
 
 if (magic[:2] == b"MZ"):
-    file.seek(0x3C, 0)
-    file.seek(int.from_bytes(file.read(4), "little") + 0x8, 0)
+    file.seek(0x3C)
+    file.seek(int.from_bytes(file.read(4), "little") + 0x8)
     print_time("PE")
 
-elif (magic == b"XE\x00\x00"):
-    file.seek(0x1C, 0)
+elif (magic == b"XE\x00\x00"): # alpha
+    file.seek(0x1C)
     print_time("XE")
-    file.seek(0x74, 0)
+    file.seek(0x74)
     print_time("PE")
 
-elif (magic == b"XBEH"):
-    file.seek(0x114, 0)
+elif (magic == b"XBEH"): # final
+    file.seek(0x114)
     print_time("XBE")
-    file.seek(0x148, 0)
+    file.seek(0x148)
     print_time("PE")
-    file.seek(0x188, 0)
+    file.seek(0x188)
     print_time("Cert")
 
-elif (magic == b"XEX?" or magic == b"XEX0"):
-    file.seek(0x103C, 0)
-    file.seek(int.from_bytes(file.read(4)) + 0x1008, 0)
+elif (magic == b"XEX?" or magic == b"XEX0"): # alpha
+    file.seek(0x103C)
+    file.seek(int.from_bytes(file.read(0x4), "little") + 0x1008)
     print_time("PE")
 
-elif (magic == b"XEX-" or magic == b"XEX1" or magic == b"XEX2"):
-    file.seek(0x14, 0)
+elif (magic == b"XEX-" or magic == b"XEX1" or magic == b"XEX2"): # beta, final
+    file.seek(0x14)
     sections = int.from_bytes(file.read(4), "big")
     for _ in range(sections):
         type = int.from_bytes(file.read(4), "big")
         offset = int.from_bytes(file.read(4), "big")
-        if type == 0x18002:
+        if (type == 0x18002):
             file.seek(offset + 0x4)
-            print_time("XEX/PE")
+            print_time("XEX")
             break
 
 else:
-    print("Couldn't determine file type")
+    print("Couldn't determine file type!")
