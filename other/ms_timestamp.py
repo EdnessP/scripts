3cdcae5fc55f7430e2dc6934b347672cceab4650
EdnessP <55930127+EdnessP@users.noreply.github.com>
2021-12-15 02:08:05 +0200

    reverted no pdb integrity warnings + cleanup

--- other/ms_timestamp.py
+++ other/ms_timestamp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Python reimplementation of  xbexexmzpe.bms  with extra features
-# Written by jason098 & Edness   2021-10-23 - 2021-12-13   v1.3.3
+# Written by jason098 & Edness   2021-10-23 - 2021-12-15   v1.3.4
 
 import argparse, os
 from datetime import datetime
@@ -11,73 +11,75 @@ args = parser.parse_args()
 
 file = open(args.file, "rb")
 magic = file.read(0x2C)
+endian = "little"
+
+def read_int(bytes):
+    return int.from_bytes(file.read(bytes), endian)
 
 def print_time(type):
-    time = int.from_bytes(file.read(0x4), "big" if (type == "XEX") else "little")
-    print(f"{type} date:".ljust(10), datetime.utcfromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S"))
+    print(f"{type} date:".ljust(10), datetime.utcfromtimestamp(read_int(0x4)).strftime("%Y-%m-%d %H:%M:%S"))
 
 def parse_pdb(word, c_date):
-    # Normally it's supposed to always be Stream 2, but a
-    # few edge cases have arisen where this isn't the case
-    # and instead it's stored in seemingly random streams.
-    file_pages = os.path.getsize(args.file) // page_size
-    if (streams != 0 and streams < pages and stream_dir != 0 and
-        stream_dir < pages * page_size and pages == file_pages):
+    # Normally it's supposed to always be Stream 2, but a few edge cases have arisen
+    # where this isn't the case and instead it's stored in seemingly random streams.
+    if (streams < pages and streams != 0 and stream_dir < pages * page_size and 
+        stream_dir != 0 and os.path.getsize(args.file) == pages * page_size):
         global stream_ptr
         for _ in range(streams):
             file.seek(stream_dir + stream_ptr)
-            stream = int.from_bytes(file.read(word), "little")
+            stream = read_int(word)
             if (stream == 0 or stream > pages):
                 break
             file.seek(stream * page_size)
-            if (int.from_bytes(file.read(0x4), "little") == c_date):
+            if (read_int(0x4) == c_date):
                 print_time("PDB")
                 return
             stream_ptr += word
-    # Fallback scan if PDB integrity checks fail
+    print("Info: PDB integrity check failed! Using fallback scan.")
     for page in range(pages):
         file.seek(page * page_size)
-        if (int.from_bytes(file.read(0x4), "little") == c_date):
+        if (read_int(0x4) == c_date):
             print_time("PDB")
             break
 
 if (magic[:2] == b"MZ"):
     file.seek(0x3C)
-    file.seek(int.from_bytes(file.read(0x4), "little"))
-    section = str(file.read(0x2), "UTF-8")
-    if (section == "PE"):
+    file.seek(read_int(0x4))
+    magic = str(file.read(0x2), "UTF-8")
+    if (magic == "PE"):
         file.seek(0x6, 1)
-        print_time(section)
+        print_time(magic)
     else: # NE, LE, LX have no timestamp
-        print(f"Unsupported {section} file.")
+        print(f"Unsupported {magic} file.")
 
 elif (magic[:4] == b"XE\x00\x00"): # alpha
     file.seek(0x1C)
     print_time("XE")
     file.seek(0x24)
-    file.seek(int.from_bytes(file.read(0x4), "little") + 0x3C)
+    file.seek(read_int(0x4) + 0x3C)
     print_time("PE")
 
 elif (magic[:4] == b"XBEH"): # final
     file.seek(0x114)
     print_time("XBE")
-    cert_offs = int.from_bytes(file.read(0x2), "little")
     file.seek(0x148)
     print_time("PE")
-    file.seek(cert_offs + 0x4)
+    file.seek(0x118)
+    file.seek(read_int(0x2) + 0x4)
     print_time("Cert")
 
 elif (magic[:4] == b"XEX?" or magic[:4] == b"XEX0"): # alpha
     file.seek(0x103C)
-    file.seek(int.from_bytes(file.read(0x4), "little") + 0x1008)
+    file.seek(read_int(0x4) + 0x1008)
     print_time("PE")
 
 elif (magic[:4] == b"XEX-" or magic[:4] == b"XEX1" or magic[:4] == b"XEX2"): # beta, final
+    endian = "big"
     file.seek(0x14)
-    sections = int.from_bytes(file.read(0x4), "big")
+    sections = read_int(0x4)
     for _ in range(sections):
-        type = int.from_bytes(file.read(0x4), "big")
-        offset = int.from_bytes(file.read(0x4), "big")
+        type = read_int(0x4)
+        offset = read_int(0x4)
         if (type == 0x18002):
             file.seek(offset + 0x4)
             print_time("XEX")
@@ -85,26 +87,26 @@ elif (magic[:4] == b"XEX-" or magic[:4] == b"XEX1" or magic[:4] == b"XEX2"): # b
 
 elif (magic == b"Microsoft C/C++ program database 2.00\r\n\x1AJG\x00\x00"):
     file.seek(0x2C)
-    page_size = int.from_bytes(file.read(0x4), "little")
+    page_size = read_int(0x4)
     file.seek(0x32)
-    pages = int.from_bytes(file.read(0x2), "little")
+    pages = read_int(0x2)
     file.seek(0x3C)
-    stream_dir = int.from_bytes(file.read(0x2), "little") * page_size
+    stream_dir = read_int(0x2) * page_size
     file.seek(stream_dir)
-    streams = int.from_bytes(file.read(0x2), "little")
+    streams = read_int(0x2)
     stream_ptr = streams * 8 + 4
     parse_pdb(0x2, 19970604)
 
 elif (magic[:32] == b"Microsoft C/C++ MSF 7.00\r\n\x1ADS\x00\x00\x00"):
     file.seek(0x20)
-    page_size = int.from_bytes(file.read(0x4), "little")
+    page_size = read_int(0x4)
     file.seek(0x28)
-    pages = int.from_bytes(file.read(0x4), "little")
+    pages = read_int(0x4)
     file.seek(0x34)
-    file.seek(int.from_bytes(file.read(0x4), "little") * page_size)
-    stream_dir = int.from_bytes(file.read(0x4), "little") * page_size
+    file.seek(read_int(0x4) * page_size)
+    stream_dir = read_int(0x4) * page_size
     file.seek(stream_dir)
-    streams = int.from_bytes(file.read(0x4), "little")
+    streams = read_int(0x4)
     stream_ptr = streams * 4 + 4
     parse_pdb(0x4, 20000404)
 
