60874fb8dbad98b610664f9034f97521bd3be26a
EdnessP <55930127+EdnessP@users.noreply.github.com>
2021-12-17 17:15:47 +0200

    v1.4 update -  Xbox One XVD / FILETIME support

--- other/ms_timestamp.py
+++ other/ms_timestamp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# Python reimplementation of  xbexexmzpe.bms  with extra features
-# Written by jason098 & Edness   2021-10-23 - 2021-12-15   v1.3.4
+# Python reimplementation of xbexexmzpe.bms with extra features
+# Written by jason098 & Edness   2021-10-23 - 2021-12-17   v1.4
 
 import argparse, os
 from datetime import datetime
@@ -10,19 +10,20 @@ parser.add_argument("file", type=str)
 args = parser.parse_args()
 
 file = open(args.file, "rb")
-magic = file.read(0x2C)
+magic = file.read(0x208)
 endian = "little"
 
 def read_int(bytes):
     return int.from_bytes(file.read(bytes), endian)
 
 def print_time(type):
-    print(f"{type} date:".ljust(10), datetime.utcfromtimestamp(read_int(0x4)).strftime("%Y-%m-%d %H:%M:%S"))
+    time = divmod(read_int(0x8) - 0x19DB1DED53E8000, 10000000)[0] if (type == "XVD") else read_int(0x4)
+    print(f"{type} date:".ljust(10), datetime.utcfromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S"))
 
 def parse_pdb(word, c_date):
     # Normally it's supposed to always be Stream 2, but a few edge cases have arisen
     # where this isn't the case and instead it's stored in seemingly random streams.
-    if (streams < pages and streams != 0 and stream_dir < pages * page_size and 
+    if (streams < pages and streams != 0 and stream_dir < pages * page_size and
         stream_dir != 0 and os.path.getsize(args.file) == pages * page_size):
         global stream_ptr
         for _ in range(streams):
@@ -42,7 +43,7 @@ def parse_pdb(word, c_date):
             print_time("PDB")
             break
 
-if (magic[:2] == b"MZ"):
+if (magic[:0x2] == b"MZ"):
     file.seek(0x3C)
     file.seek(read_int(0x4))
     magic = str(file.read(0x2), "UTF-8")
@@ -52,14 +53,14 @@ if (magic[:2] == b"MZ"):
     else: # NE, LE, LX have no timestamp
         print(f"Unsupported {magic} file.")
 
-elif (magic[:4] == b"XE\x00\x00"): # alpha
+elif (magic[:0x4] == b"XE\x00\x00"): # alpha
     file.seek(0x1C)
     print_time("XE")
     file.seek(0x24)
     file.seek(read_int(0x4) + 0x3C)
     print_time("PE")
 
-elif (magic[:4] == b"XBEH"): # final
+elif (magic[:0x4] == b"XBEH"): # final
     file.seek(0x114)
     print_time("XBE")
     file.seek(0x148)
@@ -68,12 +69,12 @@ elif (magic[:4] == b"XBEH"): # final
     file.seek(read_int(0x2) + 0x4)
     print_time("Cert")
 
-elif (magic[:4] == b"XEX?" or magic[:4] == b"XEX0"): # alpha
+elif (magic[:0x4] == b"XEX?" or magic[:0x4] == b"XEX0"): # alpha
     file.seek(0x103C)
     file.seek(read_int(0x4) + 0x1008)
     print_time("PE")
 
-elif (magic[:4] == b"XEX-" or magic[:4] == b"XEX1" or magic[:4] == b"XEX2"): # beta, final
+elif (magic[:0x4] == b"XEX-" or magic[:0x4] == b"XEX1" or magic[:0x4] == b"XEX2"): # beta, final
     endian = "big"
     file.seek(0x14)
     sections = read_int(0x4)
@@ -85,7 +86,11 @@ elif (magic[:4] == b"XEX-" or magic[:4] == b"XEX1" or magic[:4] == b"XEX2"): # b
             print_time("XEX")
             break
 
-elif (magic == b"Microsoft C/C++ program database 2.00\r\n\x1AJG\x00\x00"):
+elif (magic[0x200:0x208] == b"msft-xvd"):
+    file.seek(0x210)
+    print_time("XVD")
+
+elif (magic[:0x2C] == b"Microsoft C/C++ program database 2.00\r\n\x1AJG\x00\x00"):
     file.seek(0x2C)
     page_size = read_int(0x4)
     file.seek(0x32)
@@ -97,7 +102,7 @@ elif (magic == b"Microsoft C/C++ program database 2.00\r\n\x1AJG\x00\x00"):
     stream_ptr = streams * 8 + 4
     parse_pdb(0x2, 19970604)
 
-elif (magic[:32] == b"Microsoft C/C++ MSF 7.00\r\n\x1ADS\x00\x00\x00"):
+elif (magic[:0x20] == b"Microsoft C/C++ MSF 7.00\r\n\x1ADS\x00\x00\x00"):
     file.seek(0x20)
     page_size = read_int(0x4)
     file.seek(0x28)
