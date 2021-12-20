dc86334fe9909383edf40b8dabcfb3d3e77e3c10
EdnessP <55930127+EdnessP@users.noreply.github.com>
2021-12-20 16:06:18 +0200

    v1.4.2 update - more pdb fuckery...
    pdb v2 has THREE revisions?

--- other/ms_timestamp.py
+++ other/ms_timestamp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Python reimplementation of  xbexexmzpe.bms  with extra features
-# Written by jason098 & Edness   2021-10-23 - 2021-12-17   v1.4.1
+# Written by jason098 & Edness   2021-10-23 - 2021-12-20   v1.4.2
 
 import argparse, os
 from datetime import datetime
@@ -12,34 +12,35 @@ args = parser.parse_args()
 file = open(args.file, "rb")
 magic = file.read(0x208)
 endian = "little"
+time_t = True
 
 def read_int(bytes):
     return int.from_bytes(file.read(bytes), endian)
 
 def print_time(type):
-    time = (read_int(0x8) - 0x19DB1DED53E8000) // 10000000 if (type == "XVD") else read_int(0x4)
+    time = read_int(0x4) if (time_t == True) else (read_int(0x8) - 0x19DB1DED53E8000) // 10000000
     print(f"{type} date:".ljust(10), datetime.utcfromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S"))
 
-def parse_pdb(word, c_date):
+def parse_pdb(word, c_dates):
     # Normally it's supposed to always be Stream 2, but a few edge cases have arisen
     # where this isn't the case and instead it's stored in seemingly random streams.
     if (streams < pages and streams != 0 and stream_dir < pages * page_size and
         stream_dir != 0 and os.path.getsize(args.file) == pages * page_size):
         global stream_ptr
-        for _ in range(streams):
+        while True: # Exact amount of pointers is not specified
             file.seek(stream_dir + stream_ptr)
             stream = read_int(word)
             if (stream == 0 or stream > pages):
                 break
             file.seek(stream * page_size)
-            if (read_int(0x4) == c_date):
+            if (read_int(0x4) in c_dates):
                 print_time("PDB")
                 return
             stream_ptr += word
-    print("Info: PDB integrity check failed! Attempting fallback scan...")
+    print("Info: Something went wrong! Attempting fallback scan...")
     for page in range(pages):
         file.seek(page * page_size)
-        if (read_int(0x4) == c_date):
+        if (read_int(0x4) in c_dates):
             print_time("PDB")
             break
 
@@ -53,14 +54,14 @@ if (magic[:0x2] == b"MZ"):
     else: # NE, LE, LX have no timestamp
         print(f"Unsupported {magic} file.")
 
-elif (magic[:0x4] == b"XE\x00\x00"):
+elif (magic[:0x4] == b"XE\x00\x00"): # Xbox (Alpha)
     file.seek(0x1C)
     print_time("XE")
     file.seek(0x24)
     file.seek(read_int(0x4) + 0x3C)
     print_time("PE")
 
-elif (magic[:0x4] == b"XBEH"):
+elif (magic[:0x4] == b"XBEH"): # Xbox (Final)
     file.seek(0x114)
     print_time("XBE")
     file.seek(0x148)
@@ -69,12 +70,12 @@ elif (magic[:0x4] == b"XBEH"):
     file.seek(read_int(0x2) + 0x4)
     print_time("Cert")
 
-elif (magic[:0x4] == b"XEX?" or magic[:0x4] == b"XEX0"):
+elif (magic[:0x4] in (b"XEX?", b"XEX0")): # Xbox 360 (Alpha)
     file.seek(0x103C)
     file.seek(read_int(0x4) + 0x1008)
     print_time("PE")
 
-elif (magic[:0x4] == b"XEX-" or magic[:0x4] == b"XEX1" or magic[:0x4] == b"XEX2"):
+elif (magic[:0x4] in (b"XEX-", b"XEX1", b"XEX2")): # Xbox 360 (Beta, Final)
     endian = "big"
     file.seek(0x14)
     sections = read_int(0x4)
@@ -86,9 +87,14 @@ elif (magic[:0x4] == b"XEX-" or magic[:0x4] == b"XEX1" or magic[:0x4] == b"XEX2"
             print_time("XEX")
             break
 
-elif (magic[0x200:0x208] == b"msft-xvd"):
+elif (magic[0x200:0x208] == b"msft-xvd"): # Xbox One
+    time_t = False
     file.seek(0x210)
     print_time("XVD")
+
+elif (magic[:0x2] == b"DI"):
+    file.seek(0x8)
+    print_time("DI")
 
 elif (magic[:0x2C] == b"Microsoft C/C++ program database 2.00\r\n\x1AJG\x00\x00"):
     file.seek(0x2C)
@@ -100,7 +106,7 @@ elif (magic[:0x2C] == b"Microsoft C/C++ program database 2.00\r\n\x1AJG\x00\x00"
     file.seek(stream_dir)
     streams = read_int(0x2)
     stream_ptr = streams * 8 + 4
-    parse_pdb(0x2, 19970604)
+    parse_pdb(0x2, (19950814, 19960307, 19970604))
 
 elif (magic[:0x20] == b"Microsoft C/C++ MSF 7.00\r\n\x1ADS\x00\x00\x00"):
     file.seek(0x20)
@@ -113,7 +119,7 @@ elif (magic[:0x20] == b"Microsoft C/C++ MSF 7.00\r\n\x1ADS\x00\x00\x00"):
     file.seek(stream_dir)
     streams = read_int(0x4)
     stream_ptr = streams * 4 + 4
-    parse_pdb(0x4, 20000404)
+    parse_pdb(0x4, (20000404,))
 
 else:
     print("Couldn't determine file type!")
