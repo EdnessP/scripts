11cf8881a96b574bde097f22afbaff0819cb0dac
EdnessP <55930127+EdnessP@users.noreply.github.com>
2022-05-06 18:49:09 +0300

    Update GtHash.py

--- burnout/GtHash.py
+++ burnout/GtHash.py
@@ -2,7 +2,7 @@
 # Written by Edness
 # 2022-03-24   v1.0
 
-hash_table = (
+_hash_table = (
     0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
     0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91,
     0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7,
@@ -40,7 +40,7 @@ hash_table = (
 def calc_gthash(str):
     gt_hash = 0xFFFFFFFF
     for char in str:
-        gt_hash = gt_hash - ((gt_hash & 0x80000000) << 1) >> 8 & 0xFFFFFFFF ^ hash_table[ord(char) ^ gt_hash & 0xFF]
+        gt_hash = gt_hash - ((gt_hash & 0x80000000) << 1) >> 8 & 0xFFFFFFFF ^ _hash_table[ord(char) ^ gt_hash & 0xFF]
     return gt_hash
 
 if __name__ == "__main__":
