359e771c85c204760ce72c55227d672d4d3e3dc0
EdnessP <55930127+EdnessP@users.noreply.github.com>
2022-05-31 14:22:19 +0300

    Update tex_BombermanActZero.py

--- other/tex_BombermanActZero.py
+++ other/tex_BombermanActZero.py
@@ -1,10 +1,10 @@
-# Xbox 360 XPR2 Textures for Bomberman: Act Zero
+# Bomberman: Act Zero (Xbox 360) XPR2 textures
 
 # There are other XPR2 plugins out there, but I couldn't be bothered
 # to debug them to find out why they refused to load these textures.
 
 # Written by Edness
-# 2022-05-31   v1.0
+# 2022-05-31   v1.1
 
 XprDebug = False
 
@@ -30,63 +30,66 @@ def xprCheckType(data, isLoad=False):
     while xprOffset < chk.getSize():
         chk.seek(xprOffset)
         if chk.readBytes(4) == b"XPR2":
+            chk.seek(0x8, 1)
+            if chk.readBytes(4) != b"FOfp":
+                return False
             return True if not isLoad else xprOffset
         xprOffset += 0x1000
     return False
 
 def xprLoadTexture(data, texList):
     rapi.processCommands("-texnorepfn")
-    xprOffset = xprCheckType(data, True)
+    xprOffset = xprCheckType(data, isLoad=True)
     xpr = NoeBitStream(data[xprOffset:], NOE_BIGENDIAN)
 
     xpr.seek(0x4)
     xprHdrSize = xpr.readUInt()
     xprTexSize = xpr.readUInt()
-    # xpr.readBytes(4) == b"FOfp"
 
-    # Everything from here on out is off by 16 bytes
+    # Every pointer from here on is off by 16 bytes
     xpr = NoeBitStream(data[16 + xprOffset:], NOE_BIGENDIAN)
     texCount = xpr.readUInt()
     for tex in range(texCount):
         if xpr.readBytes(4) != b"TX2D":
             noesis.doException("Unexpected header!")
-        infoOfs = xpr.readUInt()
+
+        infoOffset = xpr.readUInt()
         xpr.seek(0x4, 1)  # 0x34?
         texSize = xpr.readUInt()
         texOffset = xpr.readUInt() + xprHdrSize
         texHeight = xpr.readUInt()
         texWidth = xpr.readUInt()
         xpr.seek(0x4, 1)
-        nameOfs = xpr.readUInt()
+        nameOffset = xpr.readUInt()
         curHdrPos = xpr.tell()
 
-        xpr.seek(nameOfs)
+        xpr.seek(nameOffset)
         texName = xpr.readString()
-        xpr.seek(infoOfs)
+        xpr.seek(infoOffset)
         # Sometimes they're 32-bit, sometimes they're 16-bit...
         infoHdr = xpr.readUInt()
         if infoHdr == 3:
-            xpr.seek(infoOfs + 0x23)
+            xpr.seek(infoOffset + 0x23)
         elif infoHdr >> 16 == 3:
-            xpr.seek(infoOfs + 0x17)
+            xpr.seek(infoOffset + 0x17)
         else:
             noesis.doException("Unhandled info header!")
         texFmt = xpr.readUByte()
 
         # The one texture I saw that had an unusual width was also not tiled.
-        # It could also be the value at infoOfs + 0x1C (in the 32-bit layout)
+        # It could also be the value at infoOffset + 0x1C (the 32-bit layout)
         # which I saw was 0x2 specifically for that texture, but I'm unsure.
         xpr.seek(texOffset)
         if texWidth > 128 and texWidth % 128 != 0 and texFmt <= 0x54:
             # Fix textures with an unusual width and a larger buffer size.
             padWidth = (texWidth // 128 + 1) * 128
             blockSize = 8 if texFmt == 0x52 else 16
-            wPad = texWidth if not texWidth % 4 else (texWidth // 4 + 1) * 4
-            hPad = texHeight if not texHeight % 4 else (texHeight // 4 + 1) * 4
+            wPad = texWidth if texWidth % 4 == 0 else (texWidth // 4 + 1) * 4
+            hPad = texHeight if texHeight % 4 == 0 else (texHeight // 4 + 1) * 4
             texData = list()
             for i in range(hPad // 4):
-                texData.extend(xpr.readBytes((wPad // 4) * (blockSize)))
-                xpr.seek(((padWidth - wPad) // 4) * (blockSize), 1)
+                texData.extend(xpr.readBytes(wPad // 4 * blockSize))
+                xpr.seek((padWidth - wPad) // 4 * blockSize, 1)
             texData = bytes(texData)
             texUntile = False
         else:
@@ -99,8 +102,8 @@ def xprLoadTexture(data, texList):
                 + "\nTexture format: 0x{:X}".format(texFmt)
                 + "\nTexture size: 0x{:X}".format(texSize)
                 + "\nTexture offset: 0x{:X}".format(texOffset + xprOffset + 16)
-                + "\nTexture name offset: 0x{:X}".format(nameOfs + xprOffset + 16)
-                + "\nTexture info offset: 0x{:X}".format(infoOfs + xprOffset + 16)
+                + "\nTexture name offset: 0x{:X}".format(nameOffset + xprOffset + 16)
+                + "\nTexture info offset: 0x{:X}".format(infoOffset + xprOffset + 16)
                 + "\nTexture {} of {}".format(tex + 1, texCount))
 
         if texFmt == 0x52:
@@ -130,4 +133,9 @@ def xprLoadTexture(data, texList):
 
         texList.append(NoeTexture(texName, texWidth, texHeight, texData, texFmt))
         xpr.seek(curHdrPos)
+
+    if XprDebug:
+        print("\nXPR offset: 0x{:X}".format(xprOffset)
+            + "\nXPR header size: 0x{:X}".format(xprHdrSize)
+            + "\nXPR data size: 0x{:X}".format(xprTexSize))
     return True
