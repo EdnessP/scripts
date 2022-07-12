6d6520a591dd789edc55c0b88d4a9ed7bc3a5690
EdnessP <55930127+EdnessP@users.noreply.github.com>
2022-07-12 07:32:12 +0300

    fix butchered comment

--- burnout/GtHash.py
+++ burnout/GtHash.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # Python reimplementation of Burnout's  CGtHash::CalculateHash  function.
 # A derivative of CRC-32 with the main difference being the output result
-# not being XORd with 0xFFFFFFFF (like CRC-32/JAMCRC) and the hash has to
-# and in the loop the hash has to be signed before shifting right 8 bits
-# and then become an unsigned integer after it as the final output.
+# not being XORd with 0xFFFFFFFF (like CRC-32/JAMCRC) and in the loop the
+# hash has to be signed before shifting right 8 bits and then become an
+# unsigned integer after it as the final output.
 
 # Written by Edness    v1.1
 # 2022-03-24  -  2022-06-20
