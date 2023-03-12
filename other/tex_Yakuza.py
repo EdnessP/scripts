4f77c8aad3c07a01278cd3c62c38eb7006456907
EdnessP <55930127+EdnessP@users.noreply.github.com>
2023-03-13 00:04:56 +0200

    fix texFmt 0x14/0x15 edge cases

--- other/tex_Yakuza.py
+++ other/tex_Yakuza.py
@@ -1,5 +1,5 @@
-# Written by Edness    v1.3
-# 2021-07-15  -  2023-03-12
+# Written by Edness    v1.4
+# 2021-07-15  -  2023-03-13
 
 from inc_noesis import *
 
@@ -36,20 +36,26 @@ def txbpLoadTex(data, texList):
 
         elif texFmt == 0x14:
             texFmt = noesis.NOESISTEX_RGBA32
-            palData = bs.readBytes(0x40)
-            texData = bs.readBytes(texWidth * texHeight // 2)
-            texData = rapi.imageDecodeRawPal(texData, palData, texWidth, texHeight, 4, "R8G8B8A8")
+            texRes = texWidth * texHeight // 2
+            texData = bs.read(texSize)
+            palData = texData[:-texRes]
+            #palData = bs.readBytes(0x40)  # Had a sample file where the palette was 0x20 bytes long instead
+            #texData = bs.readBytes(texWidth * texHeight // 2)
+            texData = rapi.imageDecodeRawPal(texData[-texRes:], palData, texWidth, texHeight, 4, "R8G8B8A8")
 
         elif texFmt == 0x15:
             texFmt = noesis.NOESISTEX_RGBA32
-            palData = bs.readBytes(0x400)
-            texData = bs.readBytes(texWidth * texHeight)
-            texData = rapi.imageDecodeRawPal(texData, palData, texWidth, texHeight, 8, "R8G8B8A8", noesis.DECODEFLAG_PS2SHIFT)
+            texRes = texWidth * texHeight
+            texData = bs.read(texSize)
+            palData = texData[:-texRes]
+            #palData = bs.readBytes(0x400)
+            #texData = bs.readBytes(texWidth * texHeight)
+            texData = rapi.imageDecodeRawPal(texData[-texRes:], palData, texWidth, texHeight, 8, "R8G8B8A8", noesis.DECODEFLAG_PS2SHIFT)
 
         elif texFmt == 0x1D:
             texFmt = noesis.NOESISTEX_RGBA32
             texData = bs.readBytes(texWidth * texHeight * 4)
-            #texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "B8G8R8A8")  # Unknown if needed, only one 0x1D sample
+            #texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "B8G8R8A8")  # Unknown if needed, only got one 0x1D sample
 
         elif texFmt == 0x23:
             # Thanks to aboood40091's GTX-Extractor for letting me know what the Gfx2 header should look like.
