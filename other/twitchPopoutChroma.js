6674e5982f407c28e8211eeea4e86f447a32ded9
EdnessP <55930127+EdnessP@users.noreply.github.com>
2023-11-17 11:51:12 +0200

    cleanup

--- other/twitchPopoutChroma.js
+++ other/twitchPopoutChroma.js
@@ -11,33 +11,33 @@
 // far better results.  Also mostly pieced together from various other scripts.
 
 if (typeof GM_addStyle == "undefined") {
-  this.GM_addStyle = (aCss) => {
-    "use strict";
-    let head = document.getElementsByTagName("head")[0];
-    if (head) {
-      let style = document.createElement("style");
-      style.textContent = aCss;
-      head.appendChild(style);
-      return style;
-    }
-    return null;
-  };
+    this.GM_addStyle = (aCss) => {
+        "use strict";
+        let head = document.getElementsByTagName("head")[0];
+        if (head) {
+            let style = document.createElement("style");
+            style.textContent = aCss;
+            head.appendChild(style);
+            return style;
+        }
+        return null;
+    };
 }
 
-GM_addStyle ( `
-  .chat-line__message, .chat-line__status, .user-notice-line {
-    text-shadow:  1px  3px 0px #000,  2px  2px 0px #000,  3px  1px 0px #000,  3px  0px 0px #000,  3px -1px 0px #000,  2px -2px 0px #000,  1px -3px 0px #000,
-                 -1px  3px 0px #000, -2px  2px 0px #000, -3px  1px 0px #000, -3px  0px 0px #000, -3px -1px 0px #000, -2px -2px 0px #000, -1px -3px 0px #000,
-                  3px  1px 0px #000,  2px  2px 0px #000,  1px  3px 0px #000,  0px  3px 0px #000, -1px  3px 0px #000, -2px  2px 0px #000, -3px  1px 0px #000,
-                  3px -1px 0px #000,  2px -2px 0px #000,  1px -3px 0px #000,  0px -3px 0px #000, -1px -3px 0px #000, -2px -2px 0px #000, -3px -1px 0px #000
-                 !important;
-    font-size: 18px;
-    font-weight: bold
+GM_addStyle(`
+    .chat-line__message, .chat-line__status, .user-notice-line {
+        text-shadow:  1px  3px 0px #000,  2px  2px 0px #000,  3px  1px 0px #000,  3px  0px 0px #000,  3px -1px 0px #000,  2px -2px 0px #000,  1px -3px 0px #000,
+                     -1px  3px 0px #000, -2px  2px 0px #000, -3px  1px 0px #000, -3px  0px 0px #000, -3px -1px 0px #000, -2px -2px 0px #000, -1px -3px 0px #000,
+                      3px  1px 0px #000,  2px  2px 0px #000,  1px  3px 0px #000,  0px  3px 0px #000, -1px  3px 0px #000, -2px  2px 0px #000, -3px  1px 0px #000,
+                      3px -1px 0px #000,  2px -2px 0px #000,  1px -3px 0px #000,  0px -3px 0px #000, -1px -3px 0px #000, -2px -2px 0px #000, -3px -1px 0px #000
+                     !important;
+        font-size: 18px;
+        font-weight: bold;
     }
-  .chat-line__timestamp {
-    font-size: 12px
+    .chat-line__timestamp {
+        font-size: 12px;
     }
-  .chat-room__content, .user-notice-line, .chat-line__message-highlight {
-    background-color: #00f !important;
+    .chat-room__content, .user-notice-line, .chat-line__message-highlight {
+        background-color: #00f !important;
     }
-` );
+`);
